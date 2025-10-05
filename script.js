25e87ff67ee93f43e1be7e852c958343ef00b8ab
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-10-05 15:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 let table = document.getElementById("sampleTable");
 
 function insert_Row() {
-    let row = document.createElement("tr");
-    let cell1 = document.createElement("td");
-    let cell2 = document.createElement("td");
+    let newRow = table.insertRow(0); 
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
 
     cell1.innerText = "New Cell1";
     cell2.innerText = "New Cell2";
-
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-
-    // insert row at beginning
-    table.prepend(row);
 }
